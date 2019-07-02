92fb3c45f1142fd66c9ee826b3e574ff109c02f8
Matthew Gencher <39591606+MattiGen@users.noreply.github.com>
2019-07-01 20:53:45 -0400

    Add pin numbers

--- montyHall.py
+++ montyHall.py
@@ -2,8 +2,13 @@ import random as rand
 import RPi.GPIO as GPIO
 
 GPIO.setwarnings(False)
-GPIO.setmode(GPIO.BCM)
-GPIO.setup()
+GPIO.setmode(GPIO.BOARD)
+GPIO.setup(29, GPIO.OUT)
+GPIO.setup(31, GPIO.OUT)
+GPIO.setup(33, GPIO.OUT)
+GPIO.setup(11, GPIO.OUT)
+GPIO.setup(13, GPIO.OUT)
+GPIO.setup(15, GPIO.OUT)
 
 #Function for swapping
 def swapChoice(closed, current):
@@ -11,7 +16,7 @@ def swapChoice(closed, current):
     while not GPIO.input():
         if GPIO.input():
             index = (index+1)%len(closed)
-            #call update lights here
+            updateChosed(closed[index])
     #Could potentially run into an issue if the button is held too long
     return closed[index]
 
@@ -20,34 +25,56 @@ def smartRemove(doors, value):
         doors.remove(value)
     
 #Update lights function
-def updateLights(openDoors, current):
-    pass
+def closeDoor(closed):
+    GPIO.output(closed, False)
     
-#DEfault pins, red: 29, blue: 33
+def updateChosen(current):
+    toggleDoors(False)
+    if current == 29:
+        GPIO.output(11, val)
+    elif current == 31:
+        GPIO.output(13, val)
+    else:
+        GPIO.output(15, val)
+    
+    
+    
+def toggleDoors(val):
+    GPIO.output(29, val)
+    GPIO.output(31, val)
+    GPIO.output(33, val)
+        
+    
+#DEfault pins, red: 29, green: 31 blue: 33
 #RGB pins: red:11, green:13, blue15
 
+
 while True:
-    break
+    toggleDorrs(True)
     #Random pick
     winDoor = random.choice()
-    doors = []
+    doors = [29,31,33]
     #Let user pick
-    current = swapChoice(doors, ) #Call lights by their pin number
+    current = swapChoice(doors, 29) #Call lights by their pin number
     #Random open
     options = doors
     smartRemove(options, winDoor)
     smartRemove(options, current)
     firstOpen = random.choice(options)
+    closeDoor(firstOpen)
     smartRemove(doors, firstOpen)
 
     #Allow user to swap
-    current = swapChoice(doors, )
-    doors = [winDoor]
+    current = swapChoice(doors, current)
+    toggleDoors(False)
+    GPIO.output(winDoor, True)
 
     #Display winner
     if winDoor == current:
-        pass
+        input("Congrats! Press any key to continue")
         #Do something, like flashing all the lights
+    else:
+        input("You Lose! Press any key to continue")
     
     
     
